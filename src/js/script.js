5c75a14f93406df7810399690c12f686a0e656de
Sławomir Toporek <toporek.slawomir@gmail.com>
2023-12-21 23:30:42 +0100

    add class Product

--- src/js/script.js
+++ src/js/script.js
@@ -7,31 +7,31 @@ const select = {
   templateOf: {
     menuProduct: "#template-menu-product",
     },
-    containerOf: {
-      menu: '#product-list',
-      cart: '#cart',
+  containerOf: {
+    menu: '#product-list',
+    cart: '#cart',
+  },
+  all: {
+    menuProducts: '#product-list > .product',
+    menuProductsActive: '#product-list > .product.active',
+    formInputs: 'input, select',
+  },
+  menuProduct: {
+    clickable: '.product__header',
+    form: '.product__order',
+    priceElem: '.product__total-price .price',
+    imageWrapper: '.product__images',
+    amountWidget: '.widget-amount',
+    cartButton: '[href="#add-to-cart"]',
+  },
+  widgets: {
+    amount: {
+      input: 'input[name="amount"]',
+      linkDecrease: 'a[href="#less"]',
+      linkIncrease: 'a[href="#more"]',
     },
-    all: {
-      menuProducts: '#product-list > .product',
-      menuProductsActive: '#product-list > .product.active',
-      formInputs: 'input, select',
-    },
-    menuProduct: {
-      clickable: '.product__header',
-      form: '.product__order',
-      priceElem: '.product__total-price .price',
-      imageWrapper: '.product__images',
-      amountWidget: '.widget-amount',
-      cartButton: '[href="#add-to-cart"]',
-    },
-    widgets: {
-      amount: {
-        input: 'input[name="amount"]',
-        linkDecrease: 'a[href="#less"]',
-        linkIncrease: 'a[href="#more"]',
-      },
-    },
-  };
+  },
+};
 
   const classNames = {
     menuProduct: {
@@ -52,7 +52,48 @@ const select = {
     menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
   };
 
+  class Product{
+    constructor(id, data){
+      const thisProduct = this;
+
+      thisProduct.id = id;
+      thisProduct.data = data;
+      thisProduct.renderInMenu();
+
+      console.log('new Product', thisProduct);
+    }
+
+    renderInMenu(){
+      const thisProduct = this;
+      const genratedHTML = templates.menuProduct(thisProduct.data);
+      console.log('generatedHTML',genratedHTML);
+      /* generate HTML based on template */
+      thisProduct.element = utils.createDOMFromHTML(genratedHTML);
+      console.log(thisProduct.element);
+      /* create element using utils.createElementFromHTML */
+      const menuContainer = document.querySelector(select.containerOf.menu);
+      /* find menu container */
+      menuContainer.appendChild(thisProduct.element);
+      /* add element to menu */
+      console.log(menuContainer);
+    }
+  }
+
   const app = {
+
+    initMenu: function(){
+      const thisApp = this;
+      console.log('thisApp.Data:', thisApp.data);
+      for(let productData in thisApp.data.products){
+        new Product(productData, thisApp.data.products[productData]);
+      }
+    },
+
+    initData: function(){
+      const thisApp = this;
+      thisApp.data = dataSource; 
+    },
+
     init: function(){
       const thisApp = this;
       console.log('*** App starting ***');
@@ -60,6 +101,9 @@ const select = {
       console.log('classNames:', classNames);
       console.log('settings:', settings);
       console.log('templates:', templates);
+
+      thisApp.initData();
+      thisApp.initMenu();
     },
   };
 
