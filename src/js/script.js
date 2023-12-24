dc2c989e5628371622e8041ebdce795e81cf1159
Sławomir Toporek <toporek.slawomir@gmail.com>
2023-12-24 18:50:24 +0100

    add method initAccordion to the class Product

--- src/js/script.js
+++ src/js/script.js
@@ -3,35 +3,35 @@
 {
   'use strict';
 
-const select = {
-  templateOf: {
-    menuProduct: "#template-menu-product",
+  const select = {
+    templateOf: {
+      menuProduct: "#template-menu-product",
+      },
+    containerOf: {
+      menu: '#product-list',
+      cart: '#cart',
     },
-  containerOf: {
-    menu: '#product-list',
-    cart: '#cart',
-  },
-  all: {
-    menuProducts: '#product-list > .product',
-    menuProductsActive: '#product-list > .product.active',
-    formInputs: 'input, select',
-  },
-  menuProduct: {
-    clickable: '.product__header',
-    form: '.product__order',
-    priceElem: '.product__total-price .price',
-    imageWrapper: '.product__images',
-    amountWidget: '.widget-amount',
-    cartButton: '[href="#add-to-cart"]',
-  },
-  widgets: {
-    amount: {
-      input: 'input[name="amount"]',
-      linkDecrease: 'a[href="#less"]',
-      linkIncrease: 'a[href="#more"]',
+    all: {
+      menuProducts: '#product-list > .product',
+      menuProductsActive: '#product-list > .product.active',
+      formInputs: 'input, select',
     },
-  },
-};
+    menuProduct: {
+      clickable: '.product__header',
+      form: '.product__order',
+      priceElem: '.product__total-price .price',
+      imageWrapper: '.product__images',
+      amountWidget: '.widget-amount',
+      cartButton: '[href="#add-to-cart"]',
+    },
+    widgets: {
+      amount: {
+        input: 'input[name="amount"]',
+        linkDecrease: 'a[href="#less"]',
+        linkIncrease: 'a[href="#more"]',
+      },
+    },
+  };
 
   const classNames = {
     menuProduct: {
@@ -59,6 +59,7 @@ const select = {
       thisProduct.id = id;
       thisProduct.data = data;
       thisProduct.renderInMenu();
+      thisProduct.initAccordion();
 
       console.log('new Product', thisProduct);
     }
@@ -66,10 +67,10 @@ const select = {
     renderInMenu(){
       const thisProduct = this;
       const genratedHTML = templates.menuProduct(thisProduct.data);
-      console.log('generatedHTML',genratedHTML);
+      console.log('generatedHTML', genratedHTML);
       /* generate HTML based on template */
       thisProduct.element = utils.createDOMFromHTML(genratedHTML);
-      console.log(thisProduct.element);
+      //console.log(thisProduct.element);
       /* create element using utils.createElementFromHTML */
       const menuContainer = document.querySelector(select.containerOf.menu);
       /* find menu container */
@@ -77,13 +78,34 @@ const select = {
       /* add element to menu */
       console.log(menuContainer);
     }
-  }
 
+    initAccordion(){
+      const thisProduct = this;
+  
+      /* find the clickable trigger (the element that should react to clicking) */
+      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      console.log('click', clickableTrigger);
+      /* START: add event listener to clickable trigger on event click */
+      clickableTrigger.addEventListener('click', function(event) {
+        /* prevent default action for event */
+        event.preventDefault();
+        /* find active product (product that has active class) */
+        const activeProduct = document.querySelector(select.all.menuProductsActive);
+        console.log('active', activeProduct);
+        /* if there is active product and it's not thisProduct.element, remove class active from it */
+        if(activeProduct && activeProduct != thisProduct.element){
+          activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
+        }
+        /* toggle active class on thisProduct.element */
+        thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
+      });
+    }
+  }
   const app = {
 
     initMenu: function(){
       const thisApp = this;
-      console.log('thisApp.Data:', thisApp.data);
+      console.log('thisApp.data:', thisApp.data);
       for(let productData in thisApp.data.products){
         new Product(productData, thisApp.data.products[productData]);
       }
