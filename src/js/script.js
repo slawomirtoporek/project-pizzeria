e05c801aee409aa8e2a5df12a011e7fc9605ddbf
Sławomir Toporek <toporek.slawomir@gmail.com>
2024-01-07 14:17:21 +0100

    add fetch to method app,initData, change method app.initMenu

--- src/js/script.js
+++ src/js/script.js
@@ -71,6 +71,11 @@
     cart: {
       defaultDeliveryFee: 20,
     },
+    db: {
+      url: '//localhost:3131',
+      products: 'products',
+      orders: 'orders',
+    },
   };
 
   const templates = {
@@ -532,13 +537,30 @@ class CartProduct {
       const thisApp = this;
       // console.log('thisApp.data:', thisApp.data);
       for(let productData in thisApp.data.products){
-        new Product(productData, thisApp.data.products[productData]);
+        new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
       }
     },
 
     initData: function(){
       const thisApp = this;
-      thisApp.data = dataSource; 
+      thisApp.data = {}; 
+
+      const url = settings.db.url + '/' + settings.db.products;
+    
+      fetch(url)
+        .then(function(rawResponse){
+          return rawResponse.json();
+        })
+        .then(function(parsedResponse){
+          console.log('parsedResponse', parsedResponse);
+
+          /* save parsedResponse as thisApp.data.products */
+          thisApp.data.products = parsedResponse;
+          /* execute initMenu method */
+          thisApp.initMenu();
+        });
+
+      console.log('thisApp.data', JSON.stringify(thisApp.data));
     },
 
     initCart: function() {
@@ -557,7 +579,6 @@ class CartProduct {
       // console.log('templates:', templates);
 
       thisApp.initData();
-      thisApp.initMenu();
       thisApp.initCart();
     },
   };
