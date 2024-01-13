ecf3779b41babc8819f855adfa1054cbe85f2b22
Sławomir Toporek <toporek.slawomir@gmail.com>
2024-01-13 20:23:33 +0100

    add the Booking class and add the render method to it

---
+++ dist/js/components/Booking.js
@@ -0,0 +1,22 @@
+import {templates} from "../settings.js";
+
+class Booking {
+
+  constructor(element){
+    const thisBooking = this;
+
+    thisBooking.render(element);
+  }
+
+  render(element){
+    const thisBooking = this;
+
+    const generatedHTML = templates.bookingWidget();
+ 
+    thisBooking.dom = {};
+    thisBooking.dom.wrapper = element;
+    thisBooking.dom.wrapper.innerHTML = generatedHTML;
+  }
+}
+
+export default Booking;
