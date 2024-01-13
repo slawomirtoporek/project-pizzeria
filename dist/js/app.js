fbc04989b9a22061045314086966a77c6956b3d5
Sławomir Toporek <toporek.slawomir@gmail.com>
2024-01-13 20:16:19 +0100

    add the initBooking method to the app object

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,6 +1,7 @@
 import {settings, select, classNames} from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
+import Booking from './components/Booking.js';
  
  const app = {
 
@@ -11,7 +12,6 @@ import Cart from './components/Cart.js';
     thisApp.navLinks = document.querySelectorAll(select.nav.links);
 
     const idFromHash = window.location.hash.replace('#/', '');
-    console.log(idFromHash);
 
     let pageMatchingHash = thisApp.pages[0].id;
 
@@ -105,6 +105,13 @@ import Cart from './components/Cart.js';
     })
   },
 
+  initBooking: function(){
+    const thisApp = this;
+
+    const bookingContainer = document.querySelector(select.containerOf.booking);
+    thisApp.booking = new Booking(bookingContainer);
+  },
+
   init: function(){
     const thisApp = this;
     // console.log('*** App starting ***');
@@ -116,6 +123,7 @@ import Cart from './components/Cart.js';
     thisApp.initPages();
     thisApp.initData();
     thisApp.initCart();
+    thisApp.initBooking();
   },
 };
 
